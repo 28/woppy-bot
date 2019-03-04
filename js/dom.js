9b032fd3d8b10b104cfb2d3fd3620337f6894f3b
Dejan Josifović <Rajoduo@yahoo.com>
2019-03-04 22:57:14 +0100

    Still WIP.

--- js/dom.js
+++ js/dom.js
@@ -1,34 +1,35 @@
+const weatherUrl = "https://woppy-bot-server.herokuapp.com/weather";
+//const weatherUrl = "http://localhost:8080/weather";
 const characterDisplayDelay = 750;
+
 let running = false;
 let intervalId = 0;
+
+function getWeather() {
+    return $.get(weatherUrl);
+}
 
 function activate() {
     if (!running) {
         clear();
         running = true;
 
-        const zipCode = getZipCode();
-        const dataProvider = getWeather(zipCode);
-        $('#activated-label').show();
-        displayZipCode(zipCode, function () {
-            $.when(dataProvider).done(function () {
-                let data = dataProvider.responseJSON;
-                if (data.query.results === null) {
-                    alert("Location not found: " + zipCode + "!");
-                } else {
-                    $('#weather-info-container').html('<h2>' + data.query.results.channel.item.title + '</h2>' + extractCData(data.query.results.channel.item.description));
-                    running = false;
-                }
-            });
+        const dataProvider = getWeather();
+        displayActivating();
+
+        $.when(dataProvider).done(function () {
+            let data = dataProvider.responseJSON;
+            let zipCode = data.zip;
+            displayZipCode(zipCode, function () {
+                displayWeather(data);
+                running = false;
+            })
         });
     }
 }
 
-function extractCData(cdata) {
-    return cdata.substr(0, (cdata.length - 3)).substr(9, cdata.length);
-}
-
 function displayZipCode(zipCode, onDone) {
+    displayActivated();
     const chars = zipCode.split('');
     const zipCodeCharacterNumber = chars.length;
     let i = 0;
@@ -43,8 +44,29 @@ function displayZipCode(zipCode, onDone) {
     }, characterDisplayDelay);
 }
 
+function displayActivated() {
+    let l = $('#activated-label');
+    l.text("Woppy activated!");
+}
+
+function displayActivating() {
+    let l = $('#activated-label');
+    l.text("Woppy activating...");
+    l.show();
+}
+
 function clear() {
     $('#zip-code-container').children('p').empty();
-    $('#weather-info-container').empty();
+    $('#weather-info-container').hide();
     $('#activated-label').hide();
+}
+
+function displayWeather(data) {
+    $('#city-info').text(data.name + ", " + data.stateCode);
+    $('#time-info').text(new Date(data.dt).toDateString());
+    $('#icon').attr("src", data.weather[0].icon);
+    $('#temp-info').text(data.main.temp + " °F");
+    $('#press-info').text(data.main.pressure + " hPa");
+    $('#hum-info').text(data.main.humidity + " %");
+    $('#weather-info-container').show();
 }
